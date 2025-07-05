6eb45e131e0d94b4d0a2da07dec0b2f8e5623c80
WilkerSebastian <wilkerafonso2005@gmail.com>
2025-07-05 01:16:47 -0400

    implementing initialize method using reinterpret_cast in an insane way to align block

--- src/GC.cpp
+++ src/GC.cpp
@@ -20,8 +20,48 @@ Allocator::Allocator()
 
 Allocator::~Allocator()
 {
+
+    if (_heap != nullptr)
+    {
+        delete[] static_cast<char*>(_heap);
+        _heap = nullptr;
+    }
+
 }
 
+void Allocator::initialize(unsigned size)
+{
+
+    if (_instance != nullptr)
+    return;
+
+    _instance = new Allocator();
+
+    _heap = new char[size];
+
+    if (_heap == nullptr)
+        throw std::bad_alloc();
+
+    BlockInfo* initialBlock = static_cast<BlockInfo*>(_heap);
+
+    const unsigned headerFooterSize = sizeof(BlockInfo) * 2;
+    const unsigned usableSize = size - headerFooterSize;
+
+    initialBlock->flag = 0;
+    initialBlock->size = usableSize;
+    initialBlock->prev = initialBlock; 
+    initialBlock->next = initialBlock;
+
+    BlockInfo* footer = reinterpret_cast<BlockInfo*>(
+        reinterpret_cast<char*>(initialBlock + 1) + usableSize
+    );
+    
+    footer->flag = 0; 
+    footer->prev = initialBlock;
+
+    _free_list_head = initialBlock;
+
+}
 
 } // end namespace tcii::ex
 
